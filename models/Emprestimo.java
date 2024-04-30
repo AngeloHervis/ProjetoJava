b25ad21d419bad70b94bb7e2cd085e84f56676b8
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-30 11:24:39 -0300

    models criados

--- models/Emprestimo.java
+++ models/Emprestimo.java
@@ -1,5 +1,58 @@
 package models;
 
+import java.time.LocalDate;
+
 public class Emprestimo {
-    
+    // Propriedades
+    private Livro livro;
+    private Usuario usuario;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucaoPrevista;
+
+    // Construtor
+    public Emprestimo(Livro livro, Usuario usuario, LocalDate dataEmprestimo, LocalDate dataDevolucaoPrevista) {
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucaoPrevista = dataDevolucaoPrevista;
+    }
+
+    // Getters e Setters
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(LocalDate dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolucaoPrevista() {
+        return dataDevolucaoPrevista;
+    }
+
+    public void setDataDevolucaoPrevista(LocalDate dataDevolucaoPrevista) {
+        this.dataDevolucaoPrevista = dataDevolucaoPrevista;
+    }
+
+    // Método para verificar se o empréstimo está atrasado
+    public boolean isAtrasado() {
+        LocalDate dataAtual = LocalDate.now();
+        return dataAtual.isAfter(dataDevolucaoPrevista);
+    }
 }
--- models/Relatorio.java
+++ models/Relatorio.java
@@ -1,5 +1,32 @@
 package models;
 
+import java.util.List;
+
 public class Relatorio {
-    
+    // Método para gerar relatório de livros emprestados
+    public void gerarRelatorioLivrosEmprestados(List<Emprestimo> emprestimos) {
+        System.out.println("Relatório de Livros Emprestados:");
+        for (Emprestimo emprestimo : emprestimos) {
+            System.out.println("Livro: " + emprestimo.getLivro().getTitulo());
+            System.out.println("Usuário: " + emprestimo.getUsuario().getNome());
+            System.out.println("Data de Empréstimo: " + emprestimo.getDataEmprestimo());
+            System.out.println("Data de Devolução Prevista: " + emprestimo.getDataDevolucaoPrevista());
+            System.out.println("----------------------------------");
+        }
+    }
+
+    // Método para gerar relatório de usuários com atrasos
+    public void gerarRelatorioUsuariosComAtrasos(List<Emprestimo> emprestimos) {
+        System.out.println("Relatório de Usuários com Atrasos:");
+        for (Emprestimo emprestimo : emprestimos) {
+            if (emprestimo.isAtrasado()) {
+                System.out.println("Usuário: " + emprestimo.getUsuario().getNome());
+                System.out.println("Livro: " + emprestimo.getLivro().getTitulo());
+                System.out.println("Data de Devolução Prevista: " + emprestimo.getDataDevolucaoPrevista());
+                System.out.println("----------------------------------");
+            }
+        }
+    }
+
+    // Outros métodos de relatório podem ser adicionados conforme necessário
 }
